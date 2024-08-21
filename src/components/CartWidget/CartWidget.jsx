3b1ff36a6614e07911f6b88c3a60b18771819050
Cristóbal Casali <ccasali94@gmail.com>
2024-08-20 23:24:56 -0400

    CartWidget y eliminar archivo innecesario

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react"
+
+const CartWidget = () => {
+    return (
+        <Box>
+            <Text>Esto es un texto</Text>
+            <Text>Numero hardcodeado</Text>
+        </Box>
+    )
+
+}
+
+export default CartWidget;
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -21,7 +21,7 @@ const NavBar = () => {
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
             <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                <Box>Logo</Box>
+                <Box>Tienda de Cristóbal</Box>
     
                 <Flex alignItems={'center'}>
                 <Stack direction={'row'} spacing={7}>
