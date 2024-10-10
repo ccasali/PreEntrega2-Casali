7c08d85b9d7fc14e9a593e39d6c8b1cfa93d3122
Cristobal Casali <ccasali94@gmail.com>
2024-10-09 23:14:19 -0300

    firebase useProductById

--- src/hooks/useProductById.jsx
+++ src/hooks/useProductById.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { getProductById } from '../services/products.service'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 
 export const useProductById = (id) => {
     const [product, setProduct] = React.useState({})
     const [loading, setLoading] = React.useState(true)
 
     React.useEffect(() => {
-        getProductById(id).then((response) => {
-            setProduct(response.data)
-        }).catch((error) => {
-            console.error(error)
-        }).finally(() => {
-            setLoading(false)
+        const productItem = doc(db, "products", id)
+        getDoc(productItem)
+            .then((snapshot) => {
+                setProduct({ id: snapshot.id, ...snapshot.data() })
         })
+        .catch((error) => {
+            console.log(error)
+        })
+        .finally(() => setLoading(false))
     }, [])
 
     return { product, loading }
--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { getAllProducts } from '../services/products.service'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
+
 
 export const useProducts = () => {
     const [products, setProducts] = React.useState([]) // 1ro estado
     const [loading, setLoading] = React.useState(true)// loading mientras espera los datos
+    const [error, setError] = React.useState(false)
 
     React.useEffect(() => {
-        getAllProducts().then((response) => {
-            setProducts(response.data.products)
-        }).catch((error) => {
-            console.error(error)
-        }).finally(() => setLoading(false))
+        const productsCollection = collection(db, 'products')
+        getDocs(productsCollection)
+            .then((snapshot) => {
+                setProducts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+            )
+        })
+        .catch((error) => setError(true))
     }, [])
 
-    return {products, loading}
+    return {products, loading, error}
 }
